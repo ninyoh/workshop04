ab2d7580eaae05eb62c348f50508afdda5ef7142
ninyoh <41226931+ninyoh@users.noreply.github.com>
2018-09-05 11:53:50 +0800

    Update pdo.php

--- pdo.php
+++ pdo.php
@@ -1,6 +1,6 @@
 <?php
 $pdo = new PDO('mysql:host=sql12.freesqldatabase.com;port=3306;dbname=sql12254594', 
-   'sql12254594', '');
+   'sql12254594', 'tQz5NKQ7jd');
 // See the "errors" folder for details...
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
