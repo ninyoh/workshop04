01b1ca8648a3366a6db510898d8405efdf5f8564
ninyoh <41226931+ninyoh@users.noreply.github.com>
2018-09-05 13:49:04 +0800

    Add files via upload

---
+++ autos2.php
@@ -0,0 +1,42 @@
+<?php
+require_once "pdo.php";
+
+if ( isset($_POST['make']) && isset($_POST['year']) 
+     && isset($_POST['mileage'])) {
+    $sql = "INSERT INTO users (make, year, mileage) 
+              VALUES (:make, :year, :mileage)";
+    echo("<pre>\n".$sql."\n</pre>\n");
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute(array(
+        ':make' => $_POST['make'],
+        ':year' => $_POST['year'],
+        ':mileage' => $_POST['mileage']));
+}
+
+$stmt = $pdo->query("SELECT make, year, mileage FROM users");
+$rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+?>
+<html>
+<head>
+    <?php require_once "bootstrap.php"; ?>
+</head>
+
+<body>
+
+<div class="container">
+    <table border="1">
+<?php
+foreach ( $rows as $row ) {
+    echo "<tr><td>";
+    echo($row['make']);
+    echo("</td><td>");
+    echo($row['year']);
+    echo("</td><td>");
+    echo($row['mileage']);
+    echo("</td></tr>\n");
+}
+?>
+</table>
+
+</body>
+
